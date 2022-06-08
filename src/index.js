78924be5e6bd3d5e5b77d6dd67f210efb6ab7500
skdding3 <skdding3@gmail.com>
2022-06-08 15:31:01 +0900

    Readme 추가 및 code 수정

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
-import { addTodo } from "./redux/actions";
+import { addTodo, completeTodo } from "./redux/actions";
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 // console.log(store);
+store.dispatch(addTodo("할 일"));
+store.dispatch(completeTodo(0));
 
-store.dispatch(addTodo("coding"));
-store.dispatch(addTodo("read book"));
-store.dispatch(addTodo("eat"));
+// store.dispatch(addTodo("coding"));
+// store.dispatch(addTodo("read book"));
+// store.dispatch(addTodo("eat"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,9 +1,10 @@
-import { ADD_TODO } from "./actions";
+import { ADD_TODO, COMPLETE_TODO, SHOW_COMPLETE } from "./actions";
 
 // state
-// ['coding', 'lunch']
-
-const initialState = [];
+// ['coding', 'lunch'];
+// [{text: '코딩', done: false}, {text: '점심', done: false}]
+// {todos: [{text: '코딩', done: false}, {text: '점심', done: false}], filter: 'ALL'}
+const initialState = { todos: [], filter: "ALL" };
 
 export function todoApp(previousState = initialState, action) {
   // 초기 값을 설정해주는 부분
@@ -12,7 +13,28 @@ export function todoApp(previousState = initialState, action) {
   // }
 
   if (action.type === ADD_TODO) {
-    return [...previousState, action.todo];
+    return {
+      ...previousState,
+      todos: [...previousState.todos, { text: action.text, done: false }],
+    };
+  }
+
+  if (action.type === COMPLETE_TODO) {
+    return {
+      ...previousState,
+      todos: previousState.todos.map((todo, index) => {
+        if (index === action.index) {
+          return { ...todo, done: true };
+        }
+        return todo;
+      }),
+    };
+  }
+
+  if (action.type === SHOW_COMPLETE) {
+    return {
+      ...previousState,
+    };
   }
 
   return previousState;
