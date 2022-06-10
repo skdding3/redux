c18ecc93b1cf81e8610d5c4d7e63bde0f3c67cb4
skdding3 <skdding3@gmail.com>
2022-06-10 12:48:16 +0900

    Redux React 연결 -1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { click } from "@testing-library/user-event/dist/click";
+import { addTodo } from "./redux/actions";
 
-function App() {
+function App({ store }) {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setState(store.getState());
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [store]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {JSON.stringify(state)}
+        <button onClick={click}>추가</button>
       </header>
     </div>
   );
+
+  function click() {
+    store.dispatch(addTodo("todo"));
+  }
 }
 
 export default App;
